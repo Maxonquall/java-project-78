95168e1054e865a41dcb4a117b3da1313d0dc60d
Maxim <msirotkin95@gmail.com>
2024-03-22 20:45:15 +0300

    Step 5

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -6,12 +6,28 @@ import hexlet.code.schemas.StringSchema;
 
 public class Validator {
     public Validator() { }
+    /**
+     * Returns a StringSchema instance that can be used to validate strings.
+     * @return StringSchema instance for validating strings.
+     */
     public StringSchema string() {
         return new StringSchema();
     }
+
+    /**
+     * Returns a NumberSchema instance that can be used to validate strings.
+     * @return StringSchema instance for validating strings.
+     */
+
     public NumberSchema number() {
         return new NumberSchema();
     }
+
+    /**
+     * Returns a MapSchema instance that can be used to validate strings.
+     * @return MapSchema instance for validating strings.
+     */
+
     public MapSchema map() {
         return new MapSchema();
     }
