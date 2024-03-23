e8a8f60b9e0b987fb1922ee3fa1bb501bc0baa17
Maxim <msirotkin95@gmail.com>
2024-03-23 12:44:46 +0300

    Step 5

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -8,7 +8,13 @@ public class MapSchema extends BaseSchema<Map> {
 
     private int mapSize = 2147483647;
     private Map<String, BaseSchema> nestedMap = new HashMap<>();
-
+    /**
+     * Validates a map according to certain criteria.
+     *
+     * @param map the map to be validated
+     * @return true if the map is valid, false otherwise
+     */
+    @Override
     public boolean isValid(Map map) {
         if (map == null) {
             return !requiredCalled;
